08b47ed0dd929d21bc40ef7c81bf7e3445ade21f
duypham0612 <duypkhe186849@fpt.edu.vn>
2025-06-19 10:41:53 +0700

    fix payment

---
+++ Models/DTO/RequestDTO/Payment/PaymentCreate.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.DTO.RequestDTO.Payment
+{
+    public class PaymentCreate
+    {
+        public int PatientId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string PaymentMethod { get; set; }
+        public string? Note { get; set; }
+    }
+}
