077c68e150d607271d736ba52f9b7713b41b32ee
ISOBEAR\Admin <haviet030404@gmail.com>
2025-06-30 02:14:18 +0700

    fix login, userProfile, register,...

--- Service/IPatientService.cs
+++ Service/IPatientService.cs
@@ -8,10 +8,13 @@ namespace SWP391_SE1914_ManageHospital.Service
         public Task<IEnumerable<PatientRespone>> GetAllPatientAsync();
         public Task<IEnumerable<PatientRespone>> SearchPatientByKeyAsync(string key);
         public Task<PatientRespone> UpdatePatientAsync(int id, PatientUpdate update);
+        public Task<PatientRespone> UpdatePatientByUserIdAsync(int userId, PatientUpdate update);
+        public Task<PatientRespone> UpdatePatientImageAsync(int userId, PatientImageUpdate imageurl);
         public Task<PatientRespone> CreatePatientAsync(PatientCreate create);
         public Task<PatientRespone> SoftDeletePatientColorAsync(int id, PatientStatus newStatus);
         public Task<bool> HardDeletePatientAsync(int id);
         public Task<PatientRespone> FindPatientByIdAsync(int id);
+        public Task<PatientRespone> FindPatientByUserIdAsync(int id);
         public Task<string> CheckUniqueCodeAsync();
         public Task<IEnumerable<PatientInfoAdmin>> PatientInfoAdAsync();
     }
