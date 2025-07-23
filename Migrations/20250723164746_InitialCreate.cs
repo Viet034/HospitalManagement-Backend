7292e66d0a7d569a293ffc856613a54989c62c65
ISOBEAR\Admin <haviet030404@gmail.com>
2025-07-23 23:54:33 +0700

    fix database

---
+++ Migrations/20250723164746_InitialCreate.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SWP391_SE1914_ManageHospital.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+    name: "Status",
+    table: "Suppliers",
+    type: "int",
+    nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiseaseDetail");
+
+            migrationBuilder.DropTable(
+                name: "Doctor_Appointment");
+
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+
+            migrationBuilder.DropTable(
+                name: "InvoiceDetail");
+
+            migrationBuilder.DropTable(
+                name: "medicine_detail");
+
+            migrationBuilder.DropTable(
+                name: "Medicine_Inventories");
+
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "Nurse_Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Payment_Invoices");
+
+            migrationBuilder.DropTable(
+                name: "PrescriptionDetails");
+
+            migrationBuilder.DropTable(
+                name: "shift_request");
+
+            migrationBuilder.DropTable(
+                name: "Supply_Inventories");
+
+            migrationBuilder.DropTable(
+                name: "User_Roles");
+
+            migrationBuilder.DropTable(
+                name: "medicine_import_details");
+
+            migrationBuilder.DropTable(
+                name: "Nurses");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "doctor_shifts");
+
+            migrationBuilder.DropTable(
+                name: "Supplies");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Medicines");
+
+            migrationBuilder.DropTable(
+                name: "medicine_imports");
+
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "MedicineCategories");
+
+            migrationBuilder.DropTable(
+                name: "Units");
+
+            migrationBuilder.DropTable(
+                name: "suppliers");
+
+            migrationBuilder.DropTable(
+                name: "Clinics");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "Medical_Records");
+
+            migrationBuilder.DropTable(
+                name: "Receptions");
+
+            migrationBuilder.DropTable(
+                name: "services");
+
+            migrationBuilder.DropTable(
+                name: "Insurances");
+
+            migrationBuilder.DropTable(
+                name: "Diseases");
+
+            migrationBuilder.DropTable(
+                name: "Prescriptions");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
