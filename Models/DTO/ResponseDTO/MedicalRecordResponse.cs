6a342df0aac74351f1367a940ed5d528e07a7879
locddhe176242 <locddhe176242@fpt.edu.vn>
2025-07-30 22:25:42 +0700

    them truong appoimentId

--- Models/DTO/ResponseDTO/MedicalRecordResponse.cs
+++ Models/DTO/ResponseDTO/MedicalRecordResponse.cs
@@ -1,9 +1,12 @@
-﻿namespace SWP391_SE1914_ManageHospital.Models.DTO.ResponseDTO;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace SWP391_SE1914_ManageHospital.Models.DTO.ResponseDTO;
 
 public class MedicalRecordResponse
 {
     // Chỉ lấy các trường cần thiết cho danh sách
     public int Id { get; set; }
+    public int AppointmentId { get; set; }
     public string Diagnosis { get; set; } = default!;
     public string Status { get; set; } = default!;
     public DateTime CreateDate { get; set; }
