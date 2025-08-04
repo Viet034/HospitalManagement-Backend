f36163cc27b7712c116f1438bd74d86052c8ea1d
ISOBEAR\Admin <haviet030404@gmail.com>
2025-08-04 07:36:20 +0700

    add Service

--- Program.cs
+++ Program.cs
@@ -98,6 +98,8 @@ builder.Services.AddScoped<IDoctorScheduleService, DoctorScheduleService>();
 builder.Services.AddScoped<IShiftRequestService, ShiftRequestService>();
 builder.Services.AddScoped<IAppointmentMapperVer2,  AppointmentMapperVer2>();
 builder.Services.AddScoped<IAppoinrmentServicever2, AppointmentServiceVer2>();
+builder.Services.AddScoped<IDoctorViewAllPatientMapper, DoctorViewAllPatientMapper>();
+builder.Services.AddScoped<IDoctorViewAllPatientService, DoctorViewAllPatientService>();
 
 
 var hash = BCrypt.Net.BCrypt.HashPassword("Admin1234$");
